752dd658bb392160bb6258accb3dfd6df0396e49
a2626 <lucas091750@gmail.com>
2018-09-26 16:24:26 +1000

    Initial commit

---
+++ app/src/main/java/com/company/insta/instagram/helper/URLS.java
@@ -0,0 +1,38 @@
+package com.company.insta.instagram.helper;
+
+/**
+ * Created by mostafa on 3/1/2018.
+ */
+
+public class URLS {
+
+    public static final String login_api = "http://lucas091750.000webhostapp.com/login.php";
+    public static final String sign_up_api = "http://lucas091750.000webhostapp.com/sign_up.php";
+    public static final String upload_story_image = "http://lucas091750.000webhostapp.com/upload_story_image.php";
+    public static final String get_following_ids = "http://lucas091750.000webhostapp.com/get_following_ids.php?user_id=";
+    public static final String lasted_news_feed = "http://lucas091750.000webhostapp.com/latest_news_feed.php?following_ids[]=";
+    public static final String all_stories_we_liked = "http://lucas091750.000webhostapp.com/all_stories_we_liked.php?story_ids[]=";
+    public static final String get_all_comments = "http://lucas091750.000webhostapp.com/get_all_comments.php?story_id=";
+    public static final String send_comment = "http://lucas091750.000webhostapp.com/send_comment.php";
+    public static final String check_following_state = "http://lucas091750.000webhostapp.com/check_following_state.php?other_user_id=";
+    public static final String unfollow_this_person = "http://lucas091750.000webhostapp.com/unfollow_this_person.php?other_user_id=";
+    public static final String follow_this_person = "http://lucas091750.000webhostapp.com/follow_this_person.php?other_user_id=";
+    public static final String get_user_data = "http://lucas091750.000webhostapp.com/get_user_data.php?user_id=";
+    public static final String get_similar_users = "http://lucas091750.000webhostapp.com/get_similar_users.php?text=";
+    public static final String get_all_images = "http://lucas091750.000webhostapp.com/get_all_images.php?id=";
+    public static final String update_profile_image = "http://lucas091750.000webhostapp.com/update_profile_image.php";
+    public static final String update_user_data = "http://lucas091750.000webhostapp.com/update_user_data.php";
+    public static final String increase_likes = "http://lucas091750.000webhostapp.com/increase_likes.php?story_id=";
+    public static final String decrease_likes = "http://lucas091750.000webhostapp.com/decrease_likes.php?story_id=";
+    public static final String add_user_to_likes_db = "http://lucas091750.000webhostapp.com/add_user_to_likes_db.php?story_id=";
+    public static final String remove_user_from_likes_db = "http://lucas091750.000webhostapp.com/remove_user_from_likes_db.php?story_id=";
+    public static final String did_user_like_story = "http://lucas091750.000webhostapp.com/did_user_like_story.php?story_id=";
+    public static final String get_all_story_ids = "http://lucas091750.000webhostapp.com/get_all_story_ids.php?user_id=";
+
+
+
+
+
+
+
+}
